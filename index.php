23fc57ffaf68c7451b90e10bcee7cec7eb7540d8
trong53 <107623849+trong53@users.noreply.github.com>
2022-12-06 09:21:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,4 @@ $controllerName = !empty($_GET['controller']) ? $_GET['controller'] : 'home';
 
 $controllerName = ucfirst(strtolower(trim($controllerName))) . 'Controller';
 
-//$action = !empty($_GET['action']) ? $_GET['action'] : '';
-
 require "./Controllers/$controllerName.php";
-
-/*
-
-controller = home ou rien  => homeController
-require 'Views/homeView.php';
-require 'Views/signinView.php';
-require 'Views/signupView.php';
-
-*/
